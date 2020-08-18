0cfc2692a6454f52ac43e7ffa674aeb4bc81680d
Schmurg <69869841+Schmurg@users.noreply.github.com>
2020-08-18 23:20:58 +0200

    Update addrow.php

--- Server_Code/addrow.php
+++ Server_Code/addrow.php
@@ -1,15 +1,11 @@
 <!-- This script adds a row to the data table. It is called 'hamilton/addrow.php?Temperature=22&Run=GettingThere' -->
 
 <html>
-   <head>
-      <title>Selecting Table in MySQLi Server</title>
-   </head>
-
    <body>
       <?php
          $dbhost = 'localhost:3306';
          $dbuser = 'datasource';
-         $dbpass = 'k9Iis8G6P0l';
+         $dbpass = 'PASSWORD';
          $dbname = 'SensorData';
          $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
    
@@ -24,8 +20,6 @@
          $BME_PRESSURE = $_GET["BME_Pressure"];
          $sql = 'INSERT INTO Data(Temperature, Run, BME_Temperature, BME_Humidity, BME_Pressure) VALUES ('.$TEMPERATURE.',\''.$RUN.'\','.$BME_TEMPERATURE.','.$BME_HUMIDITY.','.$BME_PRESSURE.');';
          $result = mysqli_query($conn, $sql);
-         if ($result === TRUE){echo 'Values inserted into database';}
-         if ($result === FALSE){echo 'Values NOT inserted into database';}   
          mysqli_close($conn);
       ?>
    </body>
