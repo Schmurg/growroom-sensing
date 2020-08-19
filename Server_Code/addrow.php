d1b0c2478f65173e3c62b49df3864bed9427ca79
Schmurg <69869841+Schmurg@users.noreply.github.com>
2020-08-19 09:18:50 +0200

    Update addrow.php

--- Server_Code/addrow.php
+++ Server_Code/addrow.php
@@ -13,12 +13,15 @@
             die('Could not connect: ' . mysqli_error());
          }
          echo 'Connected successfully<br>';
+         // Get data from HTTP request and store in local variables
          $TEMPERATURE = $_GET["Temperature"];
          $RUN = $_GET["Run"];
          $BME_TEMPERATURE = $_GET["BME_Temperature"];
          $BME_HUMIDITY = $_GET["BME_Humidity"];
          $BME_PRESSURE = $_GET["BME_Pressure"];
+         // Compose SQL query that inserts the data into the table 'Data'.
          $sql = 'INSERT INTO Data(Temperature, Run, BME_Temperature, BME_Humidity, BME_Pressure) VALUES ('.$TEMPERATURE.',\''.$RUN.'\','.$BME_TEMPERATURE.','.$BME_HUMIDITY.','.$BME_PRESSURE.');';
+         //Execute SQL query against database 'SensorData'.
          $result = mysqli_query($conn, $sql);
          mysqli_close($conn);
       ?>
